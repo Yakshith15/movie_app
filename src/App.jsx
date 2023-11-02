e25e335671e733a65c524d54bec3c15c3fa20521
Yakshith <daggupatiyakshithnaidu@gmail.com>
2023-11-02 19:49:20 +0530

    1st Commit

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+//import './App.css'
+// import {fetchDataFromApi} from './utils/api'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState,useEffect } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
+
+import { fetchDataFromApi } from './utils/api';
+
+
+
+
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import Home from "./pages/home/Home"
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
+
+
+
+
+
+function App() {
+  
+  const  dispatch=useDispatch()
+  const {url} = useSelector((state)=>
+  state.home )
+ // console.log(url);
+//Used this for testing
+  useEffect( () => {
+    fetchApiConfig();
+  }, []);
+
+  const fetchApiConfig = () => {
+    fetchDataFromApi('/configuration')
+      .then( (res) => {
+        console.log(res)
+
+        const url = {
+          backdrop : res.images.secure_base_url + "original",
+          poster : res.images.secure_base_url + "original",
+          profile : res.images.secure_base_url + "original"
+        }
+
+        dispatch(getApiConfiguration
+          (url));
+      });
+  };
+
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endPoints.forEach((url) => {
+        promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    const data = await Promise.all(promises);
+    console.log(data);
+    data.map(({ genres }) => {
+        return genres.map((item) => (allGenres[item.id] = item));
+    });
+
+    dispatch(getGenres(allGenres));
+};
+
+  return (
+    <BrowserRouter >
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    <Footer/>
+    </BrowserRouter>
+
+  );
+}
+
+export default App
